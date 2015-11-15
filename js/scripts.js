0894a30400da34479c027399459c018b13fa4b33
Filippo Facoch <facoch42@gmail.com>
2015-11-15 23:03:43 +0100

    Titolo: Finito js foo Descrizione: manca solo la lettura dei contatori dalle stampanti!

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+/*
+  DRY, cos'è DRY?
+*/
+
 function caricaPagina(){
   document.getElementById('conto1').style.display = 'none';
   document.getElementById('conto2').style.display = 'none';
@@ -5,12 +9,34 @@ function caricaPagina(){
   var salvato1=localStorage['BN1'];
   var salvato2=localStorage['BN2'];
   if(salvato1!=null){
-    document.getElementById('contatoreSalvato1').innerHTML = localStorage['BN1'];
+    document.getElementById('contatoreSalvato1').innerHTML = salvato1;
     document.getElementById('scontrino1').disabled = false;
   }
   if(salvato2!=null){
-    document.getElementById('contatoreSalvato2').innerHTML = localStorage['BN2'];
+    document.getElementById('contatoreSalvato2').innerHTML = salvato2;
     document.getElementById('scontrino2').disabled = false;
+  }
+  aggiornaStoria();
+}
+
+function aggiornaStoria(){
+  var storia1=localStorage["storiaBN1"];
+  var storia2=localStorage["storiaBN2"];
+  if(storia1!=null){
+    var out = " ";
+    storia1.split(",").forEach(function(data){
+      out= out + data + " - ";
+    });
+    out = out.slice(0, -3);
+    document.getElementById('ultimi1').innerHTML = out;
+  }
+  if(storia2!=null){
+    var out = " ";
+    storia2.split(",").forEach(function(data){
+      out= out + data + " - ";
+    });
+    out = out.slice(0, -3);
+    document.getElementById('ultimi2').innerHTML = out;
   }
 }
 
@@ -37,6 +63,13 @@ function salvaContatori(id){
     localStorage[nome] = contatore;
     document.getElementById('contatoreSalvato'+id).innerHTML = contatore;
     document.getElementById('scontrino'+id).disabled = false;
+    var nuovo = [contatore];
+    var vecchio = localStorage["storia"+nome]
+    if (vecchio!=null){
+      nuovo = nuovo.concat(vecchio.split(",")).slice(0,5);;
+    }
+    localStorage["storia"+nome]= nuovo;
+    aggiornaStoria();
   });
 }
 
@@ -65,3 +98,12 @@ function abilitaAggiorna(){
   document.getElementById('aggiornaAnimato').style.display= 'none';
   document.getElementById('aggiornaFermo').style.display= 'inline-block';
 }
+
+$(document).ready(function(){
+  $('#istruzioni').on('show.bs.collapse', function () {
+    $('#sbaglio').collapse('hide');
+  });
+  $('#sbaglio').on('show.bs.collapse', function () {
+    $('#istruzioni').collapse('hide');
+  })
+});
